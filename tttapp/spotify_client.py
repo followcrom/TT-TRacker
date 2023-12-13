71ca75d000f0b7b736afe80613627dfaec4e31a7
FollowCrom <followcrom@gmail.com>
2023-12-13 21:14:43 +0000

    sp client

--- tttapp/spotify_client.py
+++ tttapp/spotify_client.py
@@ -33,10 +33,18 @@ def spotify_callback(request):
     print("Callback")
     sp_oauth = get_spotify_oauth()
     code = request.GET.get("code")
-    print("Code: ", code)
+    print("Code:", code)
     token_info = sp_oauth.get_access_token(code)
+
+    # Logging token info before saving to session
+    print("Token info before saving to session: ", token_info)
+
     request.session["token_info"] = token_info
-    print("Token info received")
+
+    # Verify if token_info is correctly saved in session
+    saved_token_info = request.session.get("token_info", {})
+    print("Token info after saving to session: ", saved_token_info)
+
     return redirect("home")
 
 
@@ -45,15 +53,32 @@ def spotify_callback(request):
 
 def get_spotipy_client(request):
     token_info = request.session.get("token_info", {})
+
+    # Logging token info retrieved from session
+    print("Retrieved token info from session: ", token_info)
+
     if not token_info:
         print("No token info")
         return None
 
-    if time.time() > token_info["expires_at"]:
+    current_time = time.time()
+    expires_at = token_info.get("expires_at", 0)
+
+    if current_time > expires_at:
+        print("Refreshing token...")
         sp_oauth = get_spotify_oauth()
         token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
-        print("Refresh token received")
+
+        # Logging token info after refresh
+        print("Refreshed token info: ", token_info)
+
         request.session["token_info"] = token_info
+
+        # Verify if refreshed token_info is correctly saved in session
+        updated_token_info = request.session.get("token_info", {})
+        print("Token info in session after refresh: ", updated_token_info)
+    else:
+        print("Token is still valid.")
 
     return Spotify(auth=token_info["access_token"])
 
--- tttapp/views.py
+++ tttapp/views.py
@@ -21,72 +21,72 @@ from .models import TrendingTracks
 # from spotipy.oauth2 import SpotifyOAuth
 
 # -----------------------------------------
-from spotipy import Spotify
-from spotipy.oauth2 import SpotifyOAuth
-import time
-from django.conf import settings
-from django.shortcuts import redirect
+# from spotipy import Spotify
+# from spotipy.oauth2 import SpotifyOAuth
+# import time
+# from django.conf import settings
+# from django.shortcuts import redirect
 
 
-def get_spotify_oauth():
-    return SpotifyOAuth(
-        client_id=settings.SPOTIFY_CLIENT_ID,
-        client_secret=settings.SPOTIFY_CLIENT_SECRET,
-        redirect_uri=settings.SPOTIFY_REDIRECT_URI,
-        scope="user-library-read user-read-playback-state",
-    )
+# def get_spotify_oauth():
+#     return SpotifyOAuth(
+#         client_id=settings.SPOTIFY_CLIENT_ID,
+#         client_secret=settings.SPOTIFY_CLIENT_SECRET,
+#         redirect_uri=settings.SPOTIFY_REDIRECT_URI,
+#         scope="user-library-read user-read-playback-state",
+#     )
+
+
+# # -----------------------------------------
+
+
+# def spotify_auth(request):
+#     sp_oauth = get_spotify_oauth()
+#     auth_url = sp_oauth.get_authorize_url()
+#     print("Auth URL: ", auth_url)
+#     return redirect(auth_url)
+
+
+# # -----------------------------------------
+
+
+# def spotify_callback(request):
+#     print("Callback")
+#     sp_oauth = get_spotify_oauth()
+#     code = request.GET.get("code")
+#     print("Code:", code)
+#     token_info = sp_oauth.get_access_token(code)
+#     request.session["token_info"] = token_info
+#     print("Token info: ", token_info)
+#     return redirect("home")
+
+
+# # -----------------------------------------
+
+
+# def get_spotipy_client(request):
+#     token_info = request.session.get("token_info", {})
+#     if not token_info:
+#         print("No token info")
+#         return None
+
+#     if time.time() > token_info["expires_at"]:
+#         sp_oauth = get_spotify_oauth()
+#         token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
+#         print("Refresh token: ", token_info)
+#         request.session["token_info"] = token_info
+
+#     return Spotify(auth=token_info["access_token"])
 
 
 # -----------------------------------------
 
-
-def spotify_auth(request):
-    sp_oauth = get_spotify_oauth()
-    auth_url = sp_oauth.get_authorize_url()
-    print("Auth URL: ", auth_url)
-    return redirect(auth_url)
-
-
-# -----------------------------------------
-
-
-def spotify_callback(request):
-    print("Callback")
-    sp_oauth = get_spotify_oauth()
-    code = request.GET.get("code")
-    print("Code:", code)
-    token_info = sp_oauth.get_access_token(code)
-    request.session["token_info"] = token_info
-    print("Token info: ", token_info)
-    return redirect("home")
-
-
-# -----------------------------------------
-
-
-def get_spotipy_client(request):
-    token_info = request.session.get("token_info", {})
-    if not token_info:
-        print("No token info")
-        return None
-
-    if time.time() > token_info["expires_at"]:
-        sp_oauth = get_spotify_oauth()
-        token_info = sp_oauth.refresh_access_token(token_info["refresh_token"])
-        print("Refresh token: ", token_info)
-        request.session["token_info"] = token_info
-
-    return Spotify(auth=token_info["access_token"])
-
-
-# -----------------------------------------
-
-# from .spotify_client import (
-#     get_spotipy_client,
-#     get_spotify_oauth,
-#     spotify_auth,
-#     spotify_callback,
-# )
+from .spotify_client import (
+    get_spotipy_client,
+    get_spotify_oauth,
+    spotify_auth,
+    spotify_callback,
+)
 
 
 # def custom_ratelimit(*args, **kwargs):
@@ -199,10 +199,15 @@ def top_tracks_long_term(request):
 # -----------------------------------------
 from django.conf import settings
 
+import time
+
 
 @login_required
 def home(request):
     name = "Welcome to the Top Track Tracker"
+
+    current_time = time.time()
+    print("Current time: ", current_time)
 
     lastfm_api_key = settings.LASTFM_API_KEY
     lastfm_username = settings.LASTFM_USERNAME

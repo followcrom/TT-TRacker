f99e2588c4ec2cf95e332daadc3eb5d0a36b971e
FollowCrom <followcrom@gmail.com>
2023-12-14 14:31:28 +0000

    james acaster

--- tttracker/settings.py
+++ tttracker/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # ENVIROMENTAL VARIABLES
-DJANGO_SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
 SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
 SPOTIFY_REDIRECT_URI = os.environ.get("SPOTIFY_REDIRECT_URI")
